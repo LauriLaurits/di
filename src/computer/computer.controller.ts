c49cb1405e62aa41fd51c3a42aa165896a11389b
lauri <lauri.laurits@gmail.com>
2022-03-18 14:11:30 +0200

    Webhook test

--- src/computer/computer.controller.ts
+++ src/computer/computer.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Logger } from '@nestjs/common';
 import { CpuService } from '../cpu/cpu.service';
 import { DiskService } from '../disk/disk.service';
 
-@Controller('comg4343gr')
+@Controller('com43gr')
 export class ComputerController {
   private logger = new Logger('TasksController', { timestamp: true });
   constructor(

770d9e1a1002bd6ab017b50295e73931d8b7adc5
lauri <lauri.laurits@gmail.com>
2022-03-25 12:07:20 +0200

    Webhook test

---
+++ build-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  clientName: 'opus',
+  projectName: 'webhooks-sample-prj-bitbucket',
+  buildDirectory: 'build',
+  npmBuildCommand: 'build',
+  branchesToFollow: ['master'],
+};

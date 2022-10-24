475fa40aa39373dbe4939bf73287b04663d7d82d
Lauri Laurits <lauri.laurits@opusonline.co>
2022-10-24 15:50:06 +0300

    Test

--- src/power/power.service.ts
+++ src/power/power.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class PowerService {
   supplyPower(watts: number) {
-    console.log(`Supplying ${watts} worth of power.`);
+    console.log(`Sgfgupplying ${watts} worth of power.`);
   }
 }

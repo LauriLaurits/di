ccf9b8e80b91228923835166843c2e270ee4e283
Lauri Laurits <lauri.laurits@opusonline.co>
2022-11-02 11:53:07 +0200

    test_for_thirdcommit2

--- src/power/power.service.ts
+++ src/power/power.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class PowerService {
   supplyPower(watts: number) {
-    console.log(`Testtt ${watts} worth of power.`);
+    console.log(`Testtt ${watts} test_for_thirdcommit2.`);
   }
 }

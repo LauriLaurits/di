69197696a26e06b1409fca724adc8d41f432c3db
LauriLaurits <lauri.laurits@opusonline.co>
2022-11-03 13:45:54 +0200

    Deleted build config

---
+++ build-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  clientName: "ekaubamaja",
+  projectName: "magnum-storefront-proto",
+  npmBuildCommand: "build:proto",
+  branchesToFollow: ['master','release/v6.2210.2'],
+};

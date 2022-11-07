18bed84490955138d5cf4bcd95d7b717cb2a56bd
LauriLaurits <lauri.laurits@opusonline.co>
2022-11-07 11:19:57 +0200

    Whidout path

--- build-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  clientName: "ekaubamaja",
-  projectName: "magnum-storefront-proto",
-  npmBuildCommand: "build:proto",
-  branchesToFollow: ['master','release/v6.2210.2'],
-};

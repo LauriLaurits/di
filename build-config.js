85ea64095c34bf7133eca5cf4d76c28ea341605e
LauriLaurits <lauri.laurits@opusonline.co>
2022-11-07 17:01:52 +0200

    Whidout path test8

--- build-config.js
+++ build-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   clientName: 'opus',
   projectName: 'webhooks-sample-di-github',
-  buildDirectory: 'buildconfiggg',
   npmBuildCommand: 'build',
   branchesToFollow: ['master'],
 };

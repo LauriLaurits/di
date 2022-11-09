3a74216be0a3dfec93a9f1d2b4c47f4ab68dd968
LauriLaurits <lauri.laurits@opusonline.co>
2022-11-09 15:25:25 +0200

    Whidout path test8

--- build-config.js
+++ build-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   clientName: 'clientNameBC',
   projectName: 'projectNameBC',
-
   buildDirectory: 'buildDirBC',
   npmBuildCommand: 'buildCommandBCC',
-
+  branchesToFollow: ['master', 'test_for_fourth-commit4'],
 };

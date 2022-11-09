3d3693d4660cf87a9665ff77328c772301624ae9
LauriLaurits <lauri.laurits@opusonline.co>
2022-11-09 13:48:19 +0200

    Whidout path test8

--- build-config.js
+++ build-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   clientName: 'clientNameBC',
   projectName: 'projectNameBC',
-  buildDirectory: 'buildBirBC',
+  //buildDirectory: 'buildDirBC',
   npmBuildCommand: 'buildCommandBC',
   branchesToFollow: ['master'],
 };

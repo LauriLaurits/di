cf7e5b22c9665459fef62c337cf343e66489d01b
LauriLaurits <lauri.laurits@opusonline.co>
2022-11-10 11:15:27 +0200

    Whidout path test8

--- build-config.js
+++ build-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  clientName: 'clientNameBC',
-  projectName: 'projectNameBC',
-  buildDirectory: 'buildDirBC',
-  npmBuildCommand: 'buildCommandBCC',
-  branchesToFollow: ['masterr', 'test_for_fourthcommit4'],
+  clientName: 'ekaubamaja',
+  projectName: 'magnum-storefront-proto',
+  buildDirectory: 'web/www',
+  npmBuildCommand: 'build:proto',
+  branchesToFollow: ['master', 'release/v6.2212.1'],
 };

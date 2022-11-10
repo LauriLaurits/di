fbd20128e86274ec2cf9aba5fabc93f78bc299b6
LauriLaurits <lauri.laurits@opusonline.co>
2022-11-10 11:29:00 +0200

    Whidout path test8

--- build-config.js
+++ build-config.js
@@ -3,5 +3,5 @@ module.exports = {
   projectName: 'magnum-storefront-proto',
   buildDirectory: 'web/www',
   npmBuildCommand: 'build:proto',
-  branchesToFollow: ['master', 'release/v6.2212.1', 'test_for_thirdcommit2'],
+  branchesToFollow: ['master', 'release/v6.2212.1', 'test_for_thirdcommit2', 'test_for_fourthcommit4'],
 };

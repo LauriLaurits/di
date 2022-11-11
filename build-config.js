cc241949f3dd7c6684c0bd32242691d0c9ecabfb
LauriLaurits <lauri.laurits@opusonline.co>
2022-11-11 11:28:29 +0200

    Whidout path test8

--- build-config.js
+++ build-config.js
@@ -3,7 +3,7 @@ module.exports = {
   buildDirectory: 'web/www',
   npmBuildCommand: 'build:proto',
   branchesToFollow: [
-    'master7',
+    'master8',
     'release/v6.2212.1',
     'test_for_thirdcommit2',
     'test_for_fourthcommit4',

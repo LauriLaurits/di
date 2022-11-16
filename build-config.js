7566f49c2f169187ac2f1f1383911eb3e7ac2d81
LauriLaurits <lauri.laurits@gmail.com>
2022-11-16 13:31:17 +0200

    Test

--- build-config.js
+++ build-config.js
@@ -3,5 +3,5 @@ module.exports = {
   projectName: 'di',
   buildDirectory: 'build',
   npmBuildCommand: 'build:proto',
-  branchesToFollow: ['master', 'master1', 'testBranch1', 'testBranch_2'],
+  branchesToFollow: ['master', 'master4', 'testBranch1', 'testBranch_2'],
 };
